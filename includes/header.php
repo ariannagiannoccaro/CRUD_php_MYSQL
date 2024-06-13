f2ca3eb61f651a92fff9862a319ab932de820ece
Arianna Giannoccaro <123045858@upq.edu.mx>
2024-06-12 23:34:48 -0600

    font awesome

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,8 @@
     <title>PHP CRUD MYSQL</title>
 <!--BOOTSTRAP-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+<!--FONT AWESOME-->
+<script src="https://kit.fontawesome.com/803cf7e363.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
